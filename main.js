0467f3fb39fc1664c187c603b211f3dfc5309f91
Drew <bqian776@gmail.com>
2024-06-27 23:30:12 +0700

    Finished 5.5

--- main.js
+++ main.js
@@ -6,9 +6,19 @@ createApp({
     const image = ref('./assets/images/socks_green.jpg');
     const productLink = ref('https://www.camt.cmu.ac.th');
     const inStock = ref(true);
-    const inventory = ref(5);
+    const inventory = ref(100);
     const onSale = ref(true);
+    const details = ref([
+      '50% cotton',
+      '30% wool',
+      '20% polyester'
+    ]);
+    const variants = ref([
+      { id: 2234, color: 'green' },
+      { id: 2235, color: 'blue' }
+    ]);
+    const sizes = ref(['S', 'M', 'L']);
 
-    return { product, image, productLink, inStock, inventory, onSale };
+    return { product, image, productLink, inStock, inventory, onSale, details, variants, sizes };
   }
 }).mount('#app');
