71ef222c64544ee66b43650d216dd28d1d40121a
Drew <bqian776@gmail.com>
2024-06-27 23:02:29 +0700

    Finished 3.6

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-const { createApp, ref } = Vue
- 
+const { createApp, ref } = Vue;
+
 createApp({
-
   setup() {
+    const product = ref('Boots');
+    const image = ref('./assets/images/socks_green.jpg');
+    const productLink = ref('https://www.camt.cmu.ac.th');
 
-    const product = ref('Boots')
-
-    const description = ref('This course is really hard.')
-
-    return { product, description }
-
+    return { product, image, productLink };
   }
-
-}).mount('#app')
-
- 
+}).mount('#app');
