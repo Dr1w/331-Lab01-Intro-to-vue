a5c9c246faae623618a8e8ae27b30b93232575c9
Drew <bqian776@gmail.com>
2024-06-27 23:21:17 +0700

    Finished 4.9

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ createApp({
     const product = ref('Boots');
     const image = ref('./assets/images/socks_green.jpg');
     const productLink = ref('https://www.camt.cmu.ac.th');
+    const inStock = ref(true);
+    const inventory = ref(5);
+    const onSale = ref(true);
 
-    return { product, image, productLink };
+    return { product, image, productLink, inStock, inventory, onSale };
   }
 }).mount('#app');
