da437f4b8918264101f92c92f5cc1abbd886e984
Drew <bqian776@gmail.com>
2024-06-28 09:51:09 +0700

    Finished 6.7

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// main.js
 const { createApp, ref } = Vue;
 
 createApp({
@@ -14,11 +15,41 @@ createApp({
       '20% polyester'
     ]);
     const variants = ref([
-      { id: 2234, color: 'green' },
-      { id: 2235, color: 'blue' }
+      { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+      { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
     ]);
     const sizes = ref(['S', 'M', 'L']);
+    
+    const cart = ref(0);
+    function addToCart() {
+      cart.value += 1;
+    }
+    function updateImage(variantImage) {
+      image.value = variantImage;
+    }
+    function toggleInStock() {
+      inStock.value = !inStock.value;
+      if (!inStock.value) {
+        inventory.value = 0; // 将库存设为0以显示“Out of Stock”
+      } else {
+        inventory.value = 100; // 重置库存
+      }
+    }
 
-    return { product, image, productLink, inStock, inventory, onSale, details, variants, sizes };
+    return {
+      product,
+      image,
+      productLink,
+      inStock,
+      inventory,
+      onSale,
+      details,
+      variants,
+      sizes,
+      cart,
+      addToCart,
+      updateImage,
+      toggleInStock
+    };
   }
 }).mount('#app');
