7292f4138445f4c87752264ed06c6782347e858a
Drew <bqian776@gmail.com>
2024-07-03 16:02:37 +0700

    #11.6

--- main.js
+++ main.js
@@ -48,4 +48,5 @@ function removeFromCart(id) {
 app.component('product-display', productDisplay);
 app.component('product-details', productDetails);
 app.component('review-form', reviewForm);
+app.component('review-list', reviewList);
 app.mount('#app');
